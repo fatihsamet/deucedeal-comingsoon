8aea8fe20cc9faf84db3b8ffbee70d8d4ef2d957
fatihsamet <fatihsamet@gmail.com>
2025-07-16 23:38:55 +0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'court-green': '#4CAF50',
+        'court-line': '#F4F4F4'
+      }
+    },
   },
   plugins: [],
 }
